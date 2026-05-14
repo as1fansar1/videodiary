bb8074b92c988d039e243341ea6f2245e7faa26f
as1fansar1 <175651243+as1fansar1@users.noreply.github.com>
2026-05-14 21:08:16 +0800

    wire captureInstallPrompt at app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { captureInstallPrompt } from './lib/installPrompt'
+
+captureInstallPrompt()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
